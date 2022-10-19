257f7a608d2e6fcf0d90c32f1d3873efefd74d23
lijiatai <lijiataihpu@gmail.com>
2022-10-19 11:15:29 +0800

    combineReducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,18 +1,8 @@
-import { CHANGE_FOCUS } from './actionTypes'
+import { combineReducers } from 'redux'
+import { reducer as headerReducer } from '../common/header/store'
 
-const defaultState = {
-    focused: false
-}
+const reducer = combineReducers({
+    header: headerReducer
+})
 
-export default function fn(state = defaultState, action) {
-    const { type, value } = action
-    const newState = JSON.parse(JSON.stringify(state))
-    switch (type) {
-        case CHANGE_FOCUS:
-            newState.focused = value
-            break;
-        default:
-    }
-
-    return newState
-}
+export default reducer
